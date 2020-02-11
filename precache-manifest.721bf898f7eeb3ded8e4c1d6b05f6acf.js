4c25e132f8f3fbe25a5efcfc1de6a308be2fef14
Yasser Alnader <yasseralnader@gmail.com>
2020-02-10 23:55:55 -0500

    Updates

---
+++ precache-manifest.721bf898f7eeb3ded8e4c1d6b05f6acf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6784bba7d87599795dfd3fa32d74e85d",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "c414aa896afe3deb5df4",
+    "url": "/build/static/css/main.3401b5d5.chunk.css"
+  },
+  {
+    "revision": "c71f0d874434037c5c07",
+    "url": "/build/static/js/2.09728de6.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/build/static/js/2.09728de6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c414aa896afe3deb5df4",
+    "url": "/build/static/js/main.1b2433e7.chunk.js"
+  },
+  {
+    "revision": "10f63de11012f4b31b04",
+    "url": "/build/static/js/runtime-main.ff75debe.js"
+  },
+  {
+    "revision": "fcdf95aacddf60148040f0845c312b21",
+    "url": "/build/static/media/weatheringWithYouCrop1.fcdf95aa.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7fd9268cf0436e6aa401b11bf54f8367.js"
+  "/build/precache-manifest.721bf898f7eeb3ded8e4c1d6b05f6acf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
