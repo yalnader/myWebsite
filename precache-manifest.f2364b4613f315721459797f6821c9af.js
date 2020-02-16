87ff991d854867cef3fab457511200e6303375b9
Yasser Alnader <yasseralnader@gmail.com>
2020-02-16 12:57:39 -0500

    Updates

---
+++ precache-manifest.f2364b4613f315721459797f6821c9af.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb99982aa009e975f52126053e9bd8cd",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "49b921296d0a2f75a486",
+    "url": "/my-website/static/css/main.a1a92de2.chunk.css"
+  },
+  {
+    "revision": "f5c9132e28ec4a2b8166",
+    "url": "/my-website/static/js/2.f9c6c488.chunk.js"
+  },
+  {
+    "revision": "df8460d6ae7ca3d8e361c62c14b1ab60",
+    "url": "/my-website/static/js/2.f9c6c488.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49b921296d0a2f75a486",
+    "url": "/my-website/static/js/main.6bf1271f.chunk.js"
+  },
+  {
+    "revision": "0ab113bda49edd30012b",
+    "url": "/my-website/static/js/runtime-main.5ac12270.js"
+  },
+  {
+    "revision": "5dc0b2111c87c2d04b4a3beb2257d180",
+    "url": "/my-website/static/media/cloud.5dc0b211.png"
+  },
+  {
+    "revision": "e0762b78d0ac964769482437a042c358",
+    "url": "/my-website/static/media/landscape2.e0762b78.jpg"
+  },
+  {
+    "revision": "4a41490351e6d201b12556cc64ba9b65",
+    "url": "/my-website/static/media/yasser.4a414903.pdf"
+  }
+]);
